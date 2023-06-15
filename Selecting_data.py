d39da741eeb4b10ab6ab6cc1cdd10090a85159ab
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-15 19:47:54 +0100

    Accessing specific element using index only

--- Selecting_data.py
+++ Selecting_data.py
@@ -3,5 +3,6 @@ csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/I
 df = pd.read_csv(csv_path)
 
 #print(df.loc[0, 'Artist']) finding specific element
+print(df.iloc[1, 2]) # accessing specific element using iloc(using index only)
 #print(df.iloc[0:2, 0:3]) slicing dataframe
-print(df.loc[0:2, 'Artist':'Released']) 
+#print(df.loc[0:2, 'Artist':'Released']) slicing dataframe using label

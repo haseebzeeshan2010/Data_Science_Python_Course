5d58c0722decec386f1e6dca7e7fcdda7bac96c1
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-15 19:45:48 +0100

    Slicing dataframe using label

--- Selecting_data.py
+++ Selecting_data.py
@@ -2,7 +2,6 @@ import pandas as pd
 csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%204/data/TopSellingAlbums.csv'
 df = pd.read_csv(csv_path)
 
-
-#print(df.loc[0, 'Artist'])
-
-print(df.iloc[0:2, 0:3])
+#print(df.loc[0, 'Artist']) finding specific element
+#print(df.iloc[0:2, 0:3]) slicing dataframe
+print(df.loc[0:2, 'Artist':'Released']) 

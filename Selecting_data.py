5218233deee9151d1bfe1ee2632032106ddbfb6d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-15 19:44:48 +0100

    Slicing the dataframe

--- Selecting_data.py
+++ Selecting_data.py
@@ -3,4 +3,6 @@ csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/I
 df = pd.read_csv(csv_path)
 
 
-print(df.loc[0, 'Artist'])
+#print(df.loc[0, 'Artist'])
+
+print(df.iloc[0:2, 0:3])

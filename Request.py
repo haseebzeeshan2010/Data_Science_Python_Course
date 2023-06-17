6858010103a9d82309bd1f5d9802a5edd06f8d8b
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-17 12:12:46 +0100

    Organising code

--- Request.py
+++ Request.py
@@ -2,4 +2,5 @@ import requests
 url = 'https://www.ibm.com'
 r = requests.get(url)
 print(r.status_code) # return request status
-print(r.request.headers)
+print(r.request.headers) # return request headers
+print(r.request.body) # return body

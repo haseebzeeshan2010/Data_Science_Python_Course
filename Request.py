1eedfac4c5307d1eeaf0bf1b52e4347327c80a75
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-17 12:21:18 +0100

    Return the HTML in the body

--- Request.py
+++ Request.py
@@ -8,3 +8,4 @@ print(r.headers) # return dictionary of http response headers
 print(r.headers['date']) # returning the data of the request
 print(r.headers['Content-Type']) # indicates the type of data
 print(r.encoding) # return the encoding
+print(r.text[0:100]) # return the HTML in the body

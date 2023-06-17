1fbb18c29c3ba84d42f8ac88eadbdd93db2a3ad6
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-17 12:14:01 +0100

    Return dictionary of HTTP headers

--- Request.py
+++ Request.py
@@ -4,3 +4,5 @@ r = requests.get(url)
 print(r.status_code) # return request status
 print(r.request.headers) # return request headers
 print(r.request.body) # return body
+
+print(r.headers) # return dictionary of http response headers

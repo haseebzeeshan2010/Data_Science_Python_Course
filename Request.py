bb98e36ac0dea3d9f960336414511c565faacae0
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-17 12:20:27 +0100

    Returning the encoding

--- Request.py
+++ Request.py
@@ -7,3 +7,4 @@ print(r.request.body) # return body
 print(r.headers) # return dictionary of http response headers
 print(r.headers['date']) # returning the data of the request
 print(r.headers['Content-Type']) # indicates the type of data
+print(r.encoding) # return the encoding
